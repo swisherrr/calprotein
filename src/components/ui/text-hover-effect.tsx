fe777a3687a1db92f19aaed3ea390e9fa3570881
swisher <zhswisher@gmail.com>
2025-07-18 13:18:02 -0400

    updated landing page for light mode

--- src/components/ui/text-hover-effect.tsx
+++ src/components/ui/text-hover-effect.tsx
@@ -84,7 +84,7 @@ export const TextHoverEffect = ({
         textAnchor="middle"
         dominantBaseline="middle"
         strokeWidth="0.8"
-        className="fill-transparent stroke-neutral-200 font-[helvetica] text-8xl font-bold dark:stroke-neutral-800"
+        className="fill-transparent stroke-neutral-800 font-[helvetica] text-8xl font-bold dark:stroke-neutral-500"
         style={{ opacity: hovered ? 0.7 : 0 }}
       >
         {text}
@@ -95,7 +95,7 @@ export const TextHoverEffect = ({
         textAnchor="middle"
         dominantBaseline="middle"
         strokeWidth="0.8"
-        className="fill-transparent stroke-neutral-200 font-[helvetica] text-8xl font-bold dark:stroke-neutral-800"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-8xl font-bold dark:stroke-neutral-500"
         initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
         animate={{
           strokeDashoffset: 0,
