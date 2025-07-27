c8a26cebeed8de1c3e8568a9631044187999c431
swisher <zhswisher@gmail.com>
2025-07-27 19:20:25 -0400

    fix - mobile login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "gainerithm",
   description: "Data-driven fitness",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({
